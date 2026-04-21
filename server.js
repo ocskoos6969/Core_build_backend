819847ceb71763a350b897af24433cd95aa071f1
diak <diak@mesterlevel.lo>
2026-04-21 11:51:04 +0200

    bcrypt csere

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const {config} = require('./config/dotenvConfig')
+const { config } = require('./config/dotenvConfig')
 const app = require('./app')
 
 const PORT = config.PORT
 const HOST = config.HOST
 
-app.listen(PORT, HOST, () => {
+app.listen(PORT, () => {
     console.log(`Szerver IP: http://${HOST}:${PORT}`)
 })
