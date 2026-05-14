74eaf6eb5ae5bb9c9c7fd99581a0d60c039402db
Szabo-apatok <szabo.attila.krisztian.422@dszcbaross.edu.hu>
2026-05-14 21:11:18 +0200

    0514

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieparser())
 app.use(cors({
-    origin: ['http://127.0.0.1:5173', 'http://localhost:5173', 'https://core-build.netlify.app/'],
+    origin: ['http://127.0.0.1:5173', 'http://localhost:5173', 'https://core-build.netlify.app/', 'https://core-build-project.netlify.app/'],
     credentials: true
 }))
 
